f25a8b2a330feacb0f3d394bda4347e14af1dd84
Andrea <ageria20@gmail.com>
2024-09-16 13:54:57 +0200

    Initial commit

---
+++ src/test/java/ageria/u5s7l1/U5s7l1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ageria.u5s7l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5s7l1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
